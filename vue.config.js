c3ec0637d4cce0672a615befb95d0366c0481beb
KyleMitton711 <upworkvancouver@gmail.com>
2021-08-21 12:44:40 -0400

    add pwa

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,40 @@
+const { GenerateSW } = require("workbox-webpack-plugin");
+
 module.exports = {
+  publicPath: process.env.NODE_ENV === "development" ? "/vuejs-pwa/" : "/",
   transpileDependencies: ["vuetify"],
-  publicPath: "/",
   chainWebpack: (config) => {
     config.plugins.delete("prefetch");
   },
   css: {
     extract: true,
   },
+  configureWebpack: {
+    plugins: [new GenerateSW()]
+  },
+  pwa: {
+    name: 'PaCE',
+    themeColor: "#42b983",
+    msTileColor: "#42b983",
+    appleMobileWebAppCache: "yes",
+    icons: [
+      {
+        src: "./img/icons/P-icon64x64.png",
+        sizes: "64x64",
+        type: "image/png"
+      },
+      {
+        src: "./favicon-192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+    ],
+    iconPaths: {
+      favicon32: null,
+      favicon16: null,
+      appleTouchIcon: null,
+      maskIcon: null,
+      msTileImage: null
+    }
+  }
 };
